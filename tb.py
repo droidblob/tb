613b52803ec29308a4417210723519b36117822b
droidblob <droidblob@gmail.com>
2013-01-28 18:38:02 -0800

    Adding sqlite and more

--- tb.py
+++ tb.py
@@ -1,4 +1,8 @@
 import sys
+import sqlite3
+
+
+from decimal import Decimal
 from ofxparse import OfxParser
 
 '''
@@ -16,10 +20,11 @@ class Account(object):
 	def __init__(self):
 		self.number = None
 		self.balance = None
+		self.balance_date = None
 		# Put other attributes here
 
 	def __str__(self):
-		return "[Account number:{0}, balance:{1}]".format(self.number, self.balance)
+		return "[Account number:{0}, balance:{1}, balance_date:{2}]".format(self.number, self.balance, self.balance_date)
 
 class Transaction(object):
 	"""Financial transaction object"""
@@ -33,6 +38,113 @@ class Transaction(object):
 
 	def __str__(self):
 		return "[Transaction payee: {0}, type: {1}, date: {2}, amount: {3}]".format(self.payee, self.type, self.date, self.amount)
+
+
+'''
+BEGIN Sql
+'''
+
+def sqlite_adapt_decimal(d):
+	return str(d)
+
+def sqlite_convert_decimal(s):
+	return Decimal(s)
+
+class DBConnectionManager(object):
+	def __init__(self):
+		sqlite3.register_adapter(Decimal, sqlite_adapt_decimal)
+		sqlite3.register_converter("decimal", sqlite_convert_decimal)
+
+	def get_connection(self):
+		# May need to be more intelligent about this
+		return sqlite3.connect(SQLITE_DB_NAME, detect_types=sqlite3.PARSE_DECLTYPES)
+
+
+class AccountSqliteAccess(object):
+	"""Class to access sqlite-backed accounts"""
+
+	DDL_ACCOUNT_CREATE = """
+	CREATE TABLE IF NOT EXISTS
+	accounts
+	(
+		id INTEGER NOT NULL,
+		number TEXT NOT NULL,
+		balance DECIMAL NOT NULL,
+		balance_date TIMESTAMP NOT NULL,
+		PRIMARY KEY (id),
+		UNIQUE (number)
+	)
+	"""
+
+	DML_ACCOUNT_INSERT = """
+	INSERT OR IGNORE INTO accounts
+	(
+		number, balance, balance_date
+	)
+	VALUES
+	(
+		:number, :balance, :balance_date
+	)
+	"""
+
+	DML_ACCOUNT_UPDATE = """
+	UPDATE accounts
+	SET
+		balance = :balance,
+		balance_date = :balance_date
+	WHERE
+		number = :number
+	"""
+
+	def __init__(self, db):
+		self.db = db
+
+	def _setup_table(self, db_connection):
+		with db_connection:
+			c = db_connection.cursor()
+			c.execute(self.DDL_ACCOUNT_CREATE)
+			
+	
+	def insert(self, account):
+		con = self.db.get_connection()
+		try:
+			with con:
+				self._setup_table(con)
+				c = con.cursor()
+				c.execute(self.DML_ACCOUNT_INSERT, {
+					'number': account.number,
+					'balance': account.balance,
+					'balance_date': account.balance_date
+					})
+				rowcount = c.rowcount
+
+		finally:
+			con.close()
+
+		return rowcount
+
+	def update(self, account):
+		con = self.db.get_connection()
+		try:
+			with con:
+				self._setup_table(con)
+				c = con.cursor()
+				c.execute(self.DML_ACCOUNT_UPDATE, {
+					'number': account.number,
+					'balance': account.balance,
+					'balance_date': account.balance_date
+					})
+
+				rowcount = c.rowcount
+		finally:
+			con.close()
+
+		return rowcount
+
+
+'''
+END Sql
+'''
 
 '''
 BEGIN OFX
@@ -68,6 +180,7 @@ class AccountsOfxMapper(object):
 
 		account.number = data.account.number
 		account.balance = data.account.statement.balance
+		account.balance_date = data.account.statement.end_date
 		# Put other attribute mappings here
 
 		return account
@@ -113,8 +226,21 @@ def main():
 	transactions_mapper = TransactionsOfxMapper(data_loader)
 	transactions = transactions_mapper.get_transactions()
 
+	db_conn = DBConnectionManager()
+	accounts_db = AccountSqliteAccess(db_conn)
+
+	rows = 0
+	updated_rows = 0
+
+	rows = accounts_db.insert(account)
+
+	if rows <= 0:
+		updated_rows = accounts_db.update(account)
+
 	print account
 	print transactions[0]
+	print "Insert rows affected: ", rows
+	print "Update rows affected: ", updated_rows
 
 if __name__ == '__main__':
 	main()
